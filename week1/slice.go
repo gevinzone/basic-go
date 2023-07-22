4793fff93f026079d514a7a381fe9aa48fe9df9c
gevin <flyhigher139@gmail.com>
2023-07-22 21:51:02 +0800

    add shrink function

--- week1/slice.go
+++ week1/slice.go
@@ -13,5 +13,31 @@ func DeleteAt[T any](src []T, index int) ([]T, error) {
 	for i := index; i < len(src)-1; i++ {
 		src[i] = src[i+1]
 	}
-	return src[:len(src)-1], nil
+	res := Shrink(src[:len(src)-1])
+	return res, nil
+}
+
+func calCapacity(c, l int) (int, bool) {
+	if c <= 64 {
+		return c, false
+	}
+	if c > 2048 && (c/l >= 2) {
+		factor := 0.625
+		return int(float32(c) * float32(factor)), true
+	}
+	if c <= 2048 && (c/l >= 4) {
+		return c / 2, true
+	}
+	return c, false
+}
+
+func Shrink[T any](src []T) []T {
+	c, l := cap(src), len(src)
+	n, changed := calCapacity(c, l)
+	if !changed {
+		return src
+	}
+	s := make([]T, 0, n)
+	s = append(s, src...)
+	return s
 }
