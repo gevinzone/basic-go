9dfe07693ef53c21c48f9723f37e63ccd1b0fa41
gevin <flyhigher139@gmail.com>
2023-08-06 22:31:38 +0800

    user repository

---
+++ week2/webook/internal/repository/dao/user.go
@@ -0,0 +1,67 @@
+// Copyright 2023 igevin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package dao
+
+import (
+	"context"
+	"errors"
+	"github.com/go-sql-driver/mysql"
+	"gorm.io/gorm"
+	"time"
+)
+
+var (
+	ErrUserDuplicateEmail = errors.New("邮箱冲突")
+	ErrUserNotFound       = gorm.ErrRecordNotFound
+)
+
+type UserDAO struct {
+	db *gorm.DB
+}
+
+func NewUserDAO(db *gorm.DB) *UserDAO {
+	return &UserDAO{db: db}
+}
+
+func (dao *UserDAO) FindByEmail(ctx context.Context, email string) (User, error) {
+	var u User
+	err := dao.db.WithContext(ctx).Where("email=?", email).First(&u).Error
+	return u, err
+}
+
+func (dao *UserDAO) Insert(ctx context.Context, u User) error {
+	now := time.Now().UnixMilli()
+	u.Ctime = now
+	u.Utime = now
+	err := dao.db.WithContext(ctx).Create(u).Error
+	if mysqlErr, ok := err.(*mysql.MySQLError); ok {
+		const uniqueConflictsErrNo uint16 = 1062
+		if mysqlErr.Number == uniqueConflictsErrNo {
+			// 邮箱冲突
+			return ErrUserDuplicateEmail
+		}
+	}
+	return err
+}
+
+// User 直接对应数据库表结构
+// 有些人叫做 entity，有些人叫做 model，有些人叫做 PO(persistent object)
+type User struct {
+	Id       int64  `gorm:"primaryKey, autoIncrement"`
+	Email    string `gorm:"unique"`
+	Password string
+	Ctime    int64
+	Utime    int64
+}
---
+++ week2/webook/internal/repository/user.go
@@ -0,0 +1,62 @@
+// Copyright 2023 igevin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package repository
+
+import (
+	"context"
+	"github.com/gevinzone/basic-go/week2/webook/internal/domain"
+	"github.com/gevinzone/basic-go/week2/webook/internal/repository/dao"
+)
+
+var (
+	ErrUserDuplicateEmail = dao.ErrUserDuplicateEmail
+	ErrUserNotFound       = dao.ErrUserNotFound
+)
+
+type UserRepository struct {
+	dao *dao.UserDAO
+}
+
+func NewUserRepository(dao *dao.UserDAO) *UserRepository {
+	return &UserRepository{
+		dao: dao,
+	}
+}
+
+func (r *UserRepository) FindByEmail(ctx context.Context, email string) (domain.User, error) {
+	u, err := r.dao.FindByEmail(ctx, email)
+	if err != nil {
+		var user domain.User
+		return user, err
+	}
+	return domain.User{
+		Id:       u.Id,
+		Email:    u.Email,
+		Password: u.Password,
+	}, nil
+}
+
+func (r *UserRepository) Create(ctx context.Context, u domain.User) error {
+	return r.dao.Insert(ctx, dao.User{
+		Email:    u.Email,
+		Password: u.Password,
+	})
+}
+
+func (r *UserRepository) FindById(int64) {
+	// 先从 cache 里面找
+	// 再从 dao 里面找
+	// 找到了回写 cache
+}
