b3ce69ef02219d02bbdae252dd32c7d2a0e5c0a7
gevin <flyhigher139@gmail.com>
2023-09-10 19:59:38 +0800

    inorder service

--- week4/webook/internal/service/sms/failover/inorder_service.go
+++ week4/webook/internal/service/sms/failover/inorder_service.go
@@ -18,26 +18,31 @@ import (
 	"context"
 	"errors"
 	"github.com/gevinzone/basic-go/week4/webook/internal/service/sms"
-	"log"
+	"sync/atomic"
 )
 
-type StartAnewService struct {
+type InorderService struct {
 	svcs []sms.Service
+	idx  int64
 }
 
-var _ sms.Service = (*StartAnewService)(nil)
+var _ sms.Service = (*InorderService)(nil)
 
-func NewStartAnewService(svcs []sms.Service) sms.Service {
+func NewInorderService(svcs []sms.Service) sms.Service {
 	return &StartAnewService{svcs: svcs}
 }
 
-func (s *StartAnewService) Send(ctx context.Context, tpl string, args []string, numbers ...string) error {
-	for _, svc := range s.svcs {
-		err := svc.Send(ctx, tpl, args, numbers...)
+func (s *InorderService) Send(ctx context.Context, tpl string, args []string, numbers ...string) error {
+	length := int64(len(s.svcs))
+	old := atomic.LoadInt64(&s.idx)
+	idx := atomic.AddInt64(&s.idx, 1)
+	for err := s.svcs[idx].Send(ctx, tpl, args, numbers...); ; {
 		if err == nil {
 			return nil
 		}
-		log.Println(err)
+		idx = atomic.AddInt64(&s.idx, 1)
+		if idx >= old+length {
+			return errors.New("全部服务商都失败了")
+		}
 	}
-	return errors.New("全部服务商都失败了")
 }
