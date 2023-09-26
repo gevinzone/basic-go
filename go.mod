e71b9c5bcf3eedb0e6d5f9122fe2da6544431526
gevin <flyhigher139@gmail.com>
2023-09-26 23:38:31 +0800

    handle request

--- go.mod
+++ go.mod
@@ -24,6 +24,7 @@ require (
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common v1.0.730
 	github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms v1.0.730
 	go.uber.org/mock v0.2.0
+	go.uber.org/zap v1.26.0
 	golang.org/x/crypto v0.9.0
 	gorm.io/driver/mysql v1.5.1
 	gorm.io/gorm v1.25.2
@@ -66,6 +67,7 @@ require (
 	github.com/tjfoc/gmsm v1.3.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.uber.org/multierr v1.10.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/net v0.10.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
---
+++ week4/webook/pkg/ginx/middlewares/handle_req.go
@@ -0,0 +1,54 @@
+// Copyright 2023 igevin
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package middlewares
+
+import (
+	"errors"
+	"github.com/gevinzone/basic-go/week4/webook/internal/web"
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"net/http"
+)
+
+func HandleReq[T any](fn func(ctx *gin.Context, req T, uc *web.UserClaims) (Result, err error)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req T
+		err := ctx.Bind(&req)
+		if err != nil {
+			return
+		}
+
+		c, _ := ctx.Get("claims")
+		uc, ok := c.(*web.UserClaims)
+		if !ok {
+			zap.L().Error("claims 错误", zap.Error(errors.New("claims 错误")))
+			ctx.String(http.StatusOK, "系统错误")
+			return
+		}
+
+		res, err := fn(ctx, req, uc)
+		if err != nil {
+			zap.L().Error("系统异常", zap.Error(err))
+		}
+		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+type Result struct {
+	// 这个叫做业务错误码
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data any    `json:"data"`
+}
